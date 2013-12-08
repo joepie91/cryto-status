e2c90975850b645c447b586ee570a2ee050965dc
Sven Slootweg <jamsoftgamedev@gmail.com>
2013-12-08 17:54:12 +0100

    Reorganize some files, add support for units in alarm rules, support network blip detection (including more leniency for heartbeat failures), fix bug in alarm matching, remove debug output, add auto-start mechanism and killscript to cstatsd, fix deps.sh

--- deps.sh
+++ deps.sh
@@ -1,2 +1,4 @@
-apt-get install -y libzmq-dev
+#!/bin/bash
+# You need squeeze-backports if you run this on squeeze!
+apt-get install -y libzmq-dev libffi-dev
 pip install pyzmq msgpack-python pynacl pyyaml
