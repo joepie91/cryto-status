11eb81316492b9710f18358c0abdfd72d36f793f
Sven Slootweg <jamsoftgamedev@gmail.com>
2013-12-08 19:19:51 +0100

    Installation steps and bugfixes

--- deps.sh
+++ deps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # You need squeeze-backports if you run this on squeeze!
 apt-get install -y libzmq-dev libffi-dev
-pip install pyzmq msgpack-python pynacl pyyaml
+pip install pyzmq msgpack-python pynacl pyyaml psutil
