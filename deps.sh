971c5ccce3b7d6c5b1aa74a5dad93abc2895c4a2
Sven Slootweg <jamsoftgamedev@gmail.com>
2013-12-09 01:45:58 +0100

    Fixes and docs

--- deps.sh
+++ deps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # You need squeeze-backports if you run this on squeeze!
-apt-get install -y libzmq-dev libffi-dev build-essential
+apt-get install -y libzmq-dev libffi-dev build-essential python python-dev
 pip install pyzmq msgpack-python pynacl pyyaml psutil
